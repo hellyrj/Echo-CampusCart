adbd2077185de0c45773b036ecba671870345f82
hellyrj <hellyhussein1@gmail.com>
2026-03-24 15:26:09 +0300

    comment

--- backend/src/routes/vendor.routes.js
+++ backend/src/routes/vendor.routes.js
@@ -3,22 +3,22 @@ import { VendorController } from "../controllers/vendor.controller.js";
 import {authenticate} from "../middlewares/auth.middleware.js";
 
 const router = Router();
-
+//router.use(authenticate); -> it makes all routes private
 const vendorController = new VendorController();
 
-//private route
+
 router.post("/vendors", authenticate, vendorController.createVendor);
 
-//public route
+
 router.get("/vendors/nearby", vendorController.getNearbyVendors);
 
-//public route
+
 router.get("/vendors/:id",  vendorController.getVendor);
 
-//private route
+
 router.put("/vendors/:id", authenticate, vendorController.updateVendor);
 
-//private route
+
 router.delete("/vendors/:id", authenticate, vendorController.deleteVendor);
 
 export default router;
