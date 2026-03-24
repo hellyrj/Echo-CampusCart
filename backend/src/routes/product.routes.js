494a8f9ff4e5ffed0ebb55e9c317640f23fd65d4
hellyrj <hellyhussein1@gmail.com>
2026-03-24 20:03:47 +0300

    authentication based route for products

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,15 +1,16 @@
 import {Router} from 'express';
+import {authenticate} from "../middlewares/auth.middleware.js";
 import { ProductController} from '../controllers/product.controller.js';
 
 const router = Router();
 const productController = new ProductController();
 // In your routes file
-router.post('/', productController.createProduct);
+router.post('/',authenticate, productController.createProduct);
 router.get('/', productController.getProducts);
 router.get('/search', productController.searchProducts);
 router.get('/popular', productController.getPopularProducts);
 router.get('/:id', productController.getProduct);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+router.put('/:id', authenticate, productController.updateProduct);
+router.delete('/:id',authenticate, productController.deleteProduct);
 
 export default router;
