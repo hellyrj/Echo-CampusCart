cc2e0f2e52ce855bdbae02fb66c50d847f3c064f
hellyrj <hellyhussein1@gmail.com>
2026-04-06 13:09:26 +0300

    seeding universities

--- backend/src/constants/defaultUniversities.js
+++ backend/src/constants/defaultUniversities.js
@@ -1,11 +1,101 @@
 export const DEFAULT_UNIVERSITIES = [
-    "Addis Ababa University",
-    "Afar University",
-    "Bahir Dar University",
-    "Borana University",
-    "Gondar University",
-    "Jimma University",
-    "Mekelle University",
-    "Oromia State University",
-    "Southern Nations, Nationalities and Peoples University"
+    {
+        name: "Addis Ababa University",
+        location: {
+            type: "Point",
+            coordinates: [38.7578, 9.0192] // Longitude, Latitude
+        },
+        address: "Addis Ababa University, Addis Ababa",
+        city: "Addis Ababa",
+        state: "Addis Ababa",
+        country: "Ethiopia"
+    },
+    {
+        name: "Afar University",
+        location: {
+            type: "Point",
+            coordinates: [41.0192, 11.7578] // Longitude, Latitude
+        },
+        address: "Afar University, Semera",
+        city: "Semera",
+        state: "Afar Region",
+        country: "Ethiopia"
+    },
+    {
+        name: "Bahir Dar University",
+        location: {
+            type: "Point",
+            coordinates: [37.3907, 11.5742] // Longitude, Latitude
+        },
+        address: "Bahir Dar University, Bahir Dar",
+        city: "Bahir Dar",
+        state: "Amhara Region",
+        country: "Ethiopia"
+    },
+    {
+        name: "Borana University",
+        location: {
+            type: "Point",
+            coordinates: [38.2496, 5.3528] // Longitude, Latitude
+        },
+        address: "Borana University, Yabelo",
+        city: "Yabelo",
+        state: "Oromia Region",
+        country: "Ethiopia"
+    },
+    {
+        name: "Gondar University",
+        location: {
+            type: "Point",
+            coordinates: [37.4647, 12.6030] // Longitude, Latitude
+        },
+        address: "Gondar University, Gondar",
+        city: "Gondar",
+        state: "Amhara Region",
+        country: "Ethiopia"
+    },
+    {
+        name: "Jimma University",
+        location: {
+            type: "Point",
+            coordinates: [36.8233, 7.6694] // Longitude, Latitude
+        },
+        address: "Jimma University, Jimma",
+        city: "Jimma",
+        state: "Oromia Region",
+        country: "Ethiopia"
+    },
+    {
+        name: "Mekelle University",
+        location: {
+            type: "Point",
+            coordinates: [39.4766, 13.4967] // Longitude, Latitude
+        },
+        address: "Mekelle University, Mekelle",
+        city: "Mekelle",
+        state: "Tigray Region",
+        country: "Ethiopia"
+    },
+    {
+        name: "Oromia State University",
+        location: {
+            type: "Point",
+            coordinates: [36.0670, 8.5402] // Longitude, Latitude
+        },
+        address: "Oromia State University, Adama",
+        city: "Adama",
+        state: "Oromia Region",
+        country: "Ethiopia"
+    },
+    {
+        name: "Southern Nations, Nationalities and Peoples University",
+        location: {
+            type: "Point",
+            coordinates: [37.5477, 6.8681] // Longitude, Latitude
+        },
+        address: "SNNP University, Hawassa",
+        city: "Hawassa",
+        state: "Southern Nations, Nationalities and Peoples' Region",
+        country: "Ethiopia"
+    }
 ]
