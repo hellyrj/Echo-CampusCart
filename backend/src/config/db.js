5a326474aab2eab924624512adc0f90bef2ffe09
Hilina <153182546+hellyrj@users.noreply.github.com>
2026-06-04 02:18:30 -0700

    Enhance MongoDB connection with dotenv and SSL options
    Added dotenv support for environment variables and modified MongoDB connection options for production.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,18 +1,31 @@
 import mongoose from "mongoose";
 import dns from "dns";
+import dotenv from "dotenv";
+dotenv.config();
+const isProduction = process.env.NODE_ENV === 'production';
+
 /**
  * connectDB - Connect to MongoDB database
  * print connection status to console
  */
+
 export const connectDB = async() => {
     // Use public DNS servers for SRV resolution if local DNS is refusing requests.
     dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI,
+         {
         family: 4,
         serverSelectionTimeoutMS: 10000,
         connectTimeoutMS: 10000,
-    });
+        dialectOptions: isProduction? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }: {}
+    }
+);
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
 };
